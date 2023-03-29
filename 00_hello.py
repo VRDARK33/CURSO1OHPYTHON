eabd6724a354c555ff5a74af371b523445084b3b
VRDARK33 <brayan54sd@gmail.com>
2023-03-29 17:20:45 -0500

    sudiendo archivos de curso

---
+++ 00_hello.py
@@ -0,0 +1,22 @@
+# esto es un comentario : hola mundo
+#hola mundo en python
+"""esto es un comentario en varias 
+lineas """ 
+
+print("hola mundo")
+print('hola python')
+
+#se puede usar '' o "" en los print 
+
+# type es una operacion del sistema que nos permite ver el tipo de dato que es
+#ejemplo
+
+print(type(23)) #tipo int
+print(type(2.5)) #tipo float 
+print(type("hola mundo")) #tipo float
+print(type(2j+1)) #tipo complex
+print(type(False)) #tipo bool
+print(type([1,2,3])) #tipo lista
+print(type({"name": "brayan"})) #tipo dic
+print(type({9.8, 3.6, 2.5})) #tipo set
+print(type((9.8, 3.6, 2.5))) #tipo tuple
---
+++ 01_variables.py
@@ -0,0 +1,8 @@
+"""la forma en la que se nombras variables es de esta manera -> first_variable (nomenclatura snake) """
+
+my_string_variable = "mi primer string"
+print(my_string_variable)
+
+my_int_variable = 23
+print(my_int_variable)
+
